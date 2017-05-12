1b54f42948331ea0fbb165dd528813a8d5a213d0
Toyam Cox <Vaelatern@gmail.com>
2017-05-12 19:06:04 -0400

    Add non-signal exit case

--- bin/measerr.py
+++ bin/measerr.py
@@ -1,9 +1,10 @@
 #!/bin/python3
 
+th = 1
 try:
-    while True:
+    while th != 0 or m != 0:
         th = float(input("Theoretical: "))
         m = float(input("Measured: "))
-        print(((th-m)/th)*100)
+        print(abs(round(((th-m)/float(th))*100, 3)))
 except:
     print("\nGoodbye\n")
